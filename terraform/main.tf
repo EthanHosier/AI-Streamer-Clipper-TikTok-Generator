6ca2c5c2f2b5021ea8adc2af8ff719d14c762c7b
Ethan Hosier <ethanjhosier@gmail.com>
2024-12-27 15:47:40 +0000

    feat: bot now uploads clip to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,3 +142,8 @@ resource "aws_s3_bucket_acl" "logging_bucket_acl" {
   bucket     = aws_s3_bucket.logging_bucket.id
   acl        = "log-delivery-write"
 }
+
+output "cloudfront_domain_name" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.cdn.domain_name
+}
